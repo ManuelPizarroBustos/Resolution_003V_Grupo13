ace53f2621ade7fc2f293d0b084b4c34d949e59a
ManuelPizarroBustos <ma.pizarrob@duocuc.cl>
2025-12-17 17:36:22 -0300

    Delete app directory

--- app/src/main/java/com/example/appzonagamer/model/UsuarioImagen.kt
+++
@@ -1,5 +0,0 @@
-package com.example.appzonagamer.model
-
-data class UsuarioImagen (
-    val thumbnail: String,
-)
--- app/src/main/java/com/example/appzonagamer/model/UsuarioLocation.kt
+++
@@ -1,6 +0,0 @@
-package com.example.appzonagamer.model
-
-data class UsuarioLocation (
-    val city: String,
-    val state: String,
-)
